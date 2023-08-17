dbd83247c97d2942d90084583baf711623fcc41b
Yashraj Kadam <kadamyashraj2004@gmail.com>
2023-08-17 14:34:50 +0530

    class work codes

---
+++ even_no.cpp
@@ -0,0 +1,24 @@
+//Program to Check Whether a Number is Even
+
+#include<iostream>
+using namespace std;
+
+void checkEven(int num)
+{
+    if(num%2==0)
+    {
+        cout<<num<<" is even number";
+    }
+    else
+    {
+        cout<<num<<" is odd number";
+    }
+}
+
+int main(){
+    int num;
+    cout<<"Enter a number: ";
+    cin>>num;
+    checkEven(num);
+    return 0;
+}
