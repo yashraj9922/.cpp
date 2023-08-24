8ac6393395b951258201f32913656487f9cf3186
Yashraj Kadam <kadamyashraj2004@gmail.com>
2023-08-24 14:26:56 +0530

    struct in cpp

---
+++ structcpp.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+struct student
+{
+    string name;
+    int rollno;
+    int marks;
+
+    void print()
+    {
+        cout << name << endl;
+        cout << rollno << endl;
+        cout << marks << endl;
+    }
+};
+
+int main()
+{
+    student s;
+    string sname;
+    cout << "Enter the student name" << endl;
+    cin >> sname;
+    s.name = sname;
+    s.rollno = 1234;
+    s.marks = 88;
+    s.print();
+    return 0;
+}
