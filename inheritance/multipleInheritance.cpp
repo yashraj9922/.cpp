36b7acdd8f18725ba0b5d6b837e5ff04c75bdf97
Yashraj Kadam <kadamyashraj2004@gmail.com>
2023-09-14 15:12:58 +0530

    multiple inheritance

--- inheritance/multipleInheritance.cpp
+++ inheritance/multipleInheritance.cpp
@@ -1,4 +1,5 @@
-// multilevel Inheritance
+// Multiple Inheritance
+
 #include <iostream>
 using namespace std;
 
@@ -12,7 +13,7 @@ public:
     }
 };
 
-class Professor : public College
+class Professor
 {
 public:
     string pname;
@@ -22,13 +23,26 @@ public:
     }
 };
 
-class Student : public Professor
+class totalMarks
+{
+public:
+    int marks;
+    void print()
+    {
+        cout << "Total Marks: " << marks << endl;
+    }
+};
+
+class Student : public College, public Professor, public totalMarks
 {
 public:
     string sname;
     void print()
     {
         cout << "Student Name: " << sname << endl;
+        cout << "College Name: " << name << endl;
+        cout << "Professor Name: " << pname << endl;
+        cout << "Total Marks: " << marks << endl;
     }
 };
 
@@ -38,12 +52,17 @@ int main()
     s1.name = "IIIT";
     s1.pname = "Manjunath KV";
     s1.sname = "Yashraj Kadam";
+    s1.marks = 100;
     cout << "College Name: " << s1.name << endl;
     cout << "Professor Name: " << s1.pname << endl;
     cout << "Student Name: " << s1.sname << endl;
+    cout << "Total Marks: " << s1.marks << endl;
     endl(cout);
+
     s1.print();
-    s1.Professor::print();
+    endl(cout);
     s1.College::print();
+    s1.Professor::print();
+    s1.totalMarks::print();
     return 0;
 }
